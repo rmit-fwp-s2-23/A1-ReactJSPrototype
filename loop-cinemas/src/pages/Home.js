4901aea627d64e7838403e00b61a13559307040e
azac0003 <s3944964@student.rmit.edu.au>
2023-08-27 14:31:19 +1000

    New update

---
+++ loop-cinemas/src/pages/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+import App from "../App";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Redirect } from 'react-router-dom';
+
+function Home() {
+    return (
+        <div>
+            <h1>Welcome</h1>
+        </div>
+    )
+}
+
+export default Home;
