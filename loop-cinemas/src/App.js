8207d83a4e7ec998ff134f4e938dff29d805938a
AlecL14 <s3949508@student.rmit.edu.au>
2023-10-10 23:59:16 +1100

    #

--- loop-cinemas/src/App.js
+++ loop-cinemas/src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import Header from './header/Header.js';
-import Navbar from './navbar/Navbar.js';
+import Header from './components/header/Header.js';
+import Navbar from './components/navbar/Navbar.js';
 import MainContent from './main-content/Main-Content.js';
-import Footer from './footer/Footer.js';
-import Login from "./pages/Login";
-import Signup from "./pages/Signup";
+import Footer from './components/footer/Footer.js';
+import Login from "./pages/Login.js";
+import Signup from "./pages/Signup.js";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
